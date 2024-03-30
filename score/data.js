8cf6d231a0e03cc06808d9bd9c587b15d3fb52d3
DaweiX <DaweiX@outlook.com>
2024-03-30 13:01:48 +0800

    Site updated: 2024-03-30 13:01:48

--- score/data.js
+++ score/data.js
@@ -46,7 +46,10 @@ class DataHandler {
       new Sheet("Adieu", "SD", "F", "e"),
       new Sheet("Misty Eyes", "SD", "F", "e"),
       new Sheet("暁の車", "SD", "A", "e"),
-      new Sheet("La Catedral", "SD", "C", "h")
+      new Sheet("La Catedral", "SD", "C", "h"),
+      new Sheet("洋娃娃的梦", "SD", "C", "m"),
+      new Sheet("estudio de concierto", "SD", "C", "h"),
+      new Sheet("estudio de concierto No.2", "SD", "C", "h")
     ].sort((a, b) => {
       if (a.name < b.name) return -1;
       if (a.name > b.name) return 1;
